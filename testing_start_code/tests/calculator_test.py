71519bbbd97672327a8785d16ce11ae31794f7d6
Karen Stewart <kastewart2002@googlemail.com>
2021-09-14 23:07:09 +0100

    add test passing

--- testing_start_code/tests/calculator_test.py
+++ testing_start_code/tests/calculator_test.py
@@ -1,2 +1,8 @@
 import unittest 
 from src.calculator import add, divide, multiply, subtract
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(5, add(2, 3))
+
+
