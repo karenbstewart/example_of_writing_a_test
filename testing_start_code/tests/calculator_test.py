3690d3729a26562c493df8670ebfd4dccb5584c0
Karen Stewart <kastewart2002@googlemail.com>
2021-09-14 23:21:05 +0100

    divide test passed

--- testing_start_code/tests/calculator_test.py
+++ testing_start_code/tests/calculator_test.py
@@ -5,6 +5,8 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(5, add(2, 3))
 
-
     def test_subtract(self):
         self.assertEqual(3, subtract(10, 7))
+
+    def test_divide(self):
+        self.assertEqual(2, divide(10, 5))
